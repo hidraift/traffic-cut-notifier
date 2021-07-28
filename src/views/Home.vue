32f59ecbc894fa3892d880dae1f560772dd07e5d
Plubedam <pedroluisubedam@hotmail.com>
2021-07-28 20:32:31 +0200

    feat: adds Home page with integrated OMS from docker instance
    The docker OMS server runs on port 8080. Also, Bootstrap has been
    added to the project.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="container">
+    <h1 class="text-center">Traffic Cut Notifier</h1>
+      <!--src="https://www.openstreetmap.org/export/embed.html?bbox=-82.55264282226564%2C22.971353928229505%2C-82.19009399414064%2C23.200960808078566&layer=mapnik"
+      --> 
+    <Map />
+  </div> 
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script> 
+  import Map from '@/components/Map.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    components: { 
+      Map
+    }
+    
   }
-}
 </script>
